3615b0ad4b5758e1a0731f0cf794c2ba238da36e
bhaskkkarrr <bhaskarchauhan748@gmail.com>
2026-09-26 14:40:04 +0530

    checking bugs

--- ai/routers/repository_router.py
+++ ai/routers/repository_router.py
@@ -1,11 +1,27 @@
+print("REPOSITORY ROUTER: START", flush=True)
+
 from fastapi import APIRouter
+
+print("REPOSITORY ROUTER: FASTAPI IMPORTED", flush=True)
+
 from models.models import RepositoryVectorDBRequest
+
+print("REPOSITORY ROUTER: MODEL IMPORTED", flush=True)
+
 from services.load_repo import load
+
+print("REPOSITORY ROUTER: LOAD_REPO IMPORTED", flush=True)
+
 repositoryRouter = APIRouter()
 
 @repositoryRouter.post('/load')
-async def load_repo(data:RepositoryVectorDBRequest):
-  print("DATA:\n",data)
-  response = await load(data.repositoryPath, data.repo_id, data.user_id)
-  return response
+async def load_repo(data: RepositoryVectorDBRequest):
+    print("DATA:", data)
+    response = await load(
+        data.repositoryPath,
+        data.repo_id,
+        data.user_id
+    )
+    return response
 
+print("REPOSITORY ROUTER: FINISHED", flush=True)
